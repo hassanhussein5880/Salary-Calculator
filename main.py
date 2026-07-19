783b34827a499c7acd4a624a2e3feb7e6f1cbbbd
Hassan <hahuahmed58@gmail.com>
2026-07-19 09:50:33 +0300

    salary calculator

---
+++ main.py
@@ -0,0 +1,1 @@
+# A simple Salary Calculation Project
---
+++ salary_receipt.py
@@ -0,0 +1,16 @@
+name = input("Enter your name:")
+hours_worked = int(input("Enter hours worked:"))
+hourly_rate = int(input("Enter hourly rate:"))
+Gross_pay = hours_worked * hourly_rate
+Tax = 22 /100
+Tax_deducted= Gross_pay * Tax
+Net_pay = Gross_pay - Tax_deducted
+
+
+
+print(f"Employee: {name}")
+print(f"Hours: {hours_worked}")
+print(f"Rate: {hourly_rate}")
+print(f"Gross Salary: {Gross_pay}")
+print(f"Tax 22%: {Tax_deducted}")
+print(f"Net Pay: {Net_pay}")
